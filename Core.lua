46a317591c7555df270c47b7da054215a59f85d2
Frank Bosman <fbushman@gmail.com>
2024-10-28 23:43:26 +0100

    Darkened roster explorer background

--- Core.lua
+++ Core.lua
@@ -107,7 +107,7 @@ SwiftdawnRaidTools.defaults = {
                 playerFontType = "Friz Quadrata TT",
                 playerFontSize = 10,
                 titleBarOpacity = 0.8,
-                backgroundOpacity = 0.4,
+                backgroundOpacity = 0.6,
                 iconSize = 14
             }
         }
