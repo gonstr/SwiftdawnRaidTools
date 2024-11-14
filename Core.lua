3d849f329a05059cd05c465c1e235ec7924c5e30
Frank Bosman <fbushman@gmail.com>
2024-11-14 05:17:16 +0100

    Fixed default appearance

--- Core.lua
+++ Core.lua
@@ -7,7 +7,7 @@ SwiftdawnRaidTools.PREFIX_SYNC = "SRT-S"
 SwiftdawnRaidTools.PREFIX_SYNC_PROGRESS = "SRT-SP"
 SwiftdawnRaidTools.PREFIX_MAIN = "SRT-M"
 
-SwiftdawnRaidTools.VERSION = GetAddOnMetadata("SwiftdawnRaidTools", "Version")
+SwiftdawnRaidTools.VERSION = C_AddOns.GetAddOnMetadata("SwiftdawnRaidTools", "Version")
 SwiftdawnRaidTools.IS_DEV = SwiftdawnRaidTools.VERSION == '\@project-version\@'
 
 -- AceDB defaults
@@ -30,11 +30,11 @@ SwiftdawnRaidTools.defaults = {
             appearance = {
                 scale = 1.2,
                 headerFontType = "Friz Quadrata TT",
-                headerFontSize = 10,
+                headerFontSize = 14,
                 playerFontType = "Friz Quadrata TT",
-                playerFontSize = 10,
+                playerFontSize = 12,
                 countdownFontType = "Friz Quadrata TT",
-                countdownFontSize = 10,
+                countdownFontSize = 12,
                 backgroundOpacity = 0.9,
                 iconSize = 16
             }
@@ -74,42 +74,6 @@ SwiftdawnRaidTools.defaults = {
                 backgroundOpacity = 0.4,
                 iconSize = 14
             }
-        },
-        assignmentexplorer = {
-            anchorX = GetScreenWidth()/2,
-            anchorY = -(GetScreenHeight()/2),
-            locked = false,
-            show = false,
-            appearance = {
-                scale = 1.0,
-                titleFontType = "Friz Quadrata TT",
-                titleFontSize = 10,
-                headerFontType = "Friz Quadrata TT",
-                headerFontSize = 10,
-                playerFontType = "Friz Quadrata TT",
-                playerFontSize = 10,
-                titleBarOpacity = 0.8,
-                backgroundOpacity = 0.6,
-                iconSize = 14
-            }
-        },
-        rosterexplorer = {
-            anchorX = GetScreenWidth()/2,
-            anchorY = -(GetScreenHeight()/2),
-            locked = false,
-            show = false,
-            appearance = {
-                scale = 1.0,
-                titleFontType = "Friz Quadrata TT",
-                titleFontSize = 10,
-                headerFontType = "Friz Quadrata TT",
-                headerFontSize = 10,
-                playerFontType = "Friz Quadrata TT",
-                playerFontSize = 10,
-                titleBarOpacity = 0.8,
-                backgroundOpacity = 0.6,
-                iconSize = 14
-            }
         }
     },
 }
@@ -126,12 +90,6 @@ function SwiftdawnRaidTools:OnInitialize()
 
     self.debugLog = SRTDebugLog:New(100, 400)
     self.debugLog:Initialize()
-
-    self.assignmentExplorer = AssignmentExplorer:New(600)
-    self.assignmentExplorer:Initialize()
-
-    self.rosterExplorer = RosterExplorer:New(600)
-    self.rosterExplorer:Initialize()
 
     self:RegisterComm(self.PREFIX_SYNC)
     self:RegisterComm(self.PREFIX_SYNC_PROGRESS)
