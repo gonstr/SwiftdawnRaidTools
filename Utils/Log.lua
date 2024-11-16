853615dc7253e437bb2a6694b762c9069257f223
Frank Bosman <fbushman@gmail.com>
2024-11-16 17:31:19 +0100

    Added log facility

---
+++ Utils/Log.lua
@@ -0,0 +1,12 @@
+local SwiftdawnRaidTools = SwiftdawnRaidTools
+Log = {}
+
+function Log.info(message, ...)
+    SwiftdawnRaidTools:Print(message, ...)
+end
+
+function Log.debug(message, ...)
+    if SwiftdawnRaidTools.DEBUG then
+        SwiftdawnRaidTools:Print(" [DEBUG] "..message, ...)
+    end
+end
