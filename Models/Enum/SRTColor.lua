af3dfeae62366de8cf7008b3b794dcd82f5d4d77
Frank Bosman <fbushman@gmail.com>
2024-11-17 01:19:02 +0100

    Added blue color

--- Models/Enum/SRTColor.lua
+++ Models/Enum/SRTColor.lua
@@ -3,6 +3,8 @@ SRTColor = {
     RedHighlight = { r=1, g=0.3, b=0.3, a=1 },
     Green = { r=0.3, g=0.8, b=0.3, a=0.8 },
     GreenHighlight = { r=0.3, g=1, b=0.3, a=1 },
+    Blue = { r=0.3, g=0.3, b=0.8, a=0.8 },
+    BlueHighlight = { r=0.3, g=0.3, b=1, a=1 },
     Gray = { r=0.3, g=0.3, b=0.3, a=0.8 },
     GrayHighlight = { r=0.8, g=0.8, b=0.8, a=1 },
     LightGray = { r=1, g=1, b=1, a=0.8},
