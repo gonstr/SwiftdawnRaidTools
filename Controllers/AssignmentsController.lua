7b855e2bc4939cec8ed88d8ee88f2409168b88ed
Frank Bosman <fbushman@gmail.com>
2024-11-30 11:26:40 +0100

    Fixed encounter print

--- Controllers/AssignmentsController.lua
+++ Controllers/AssignmentsController.lua
@@ -169,7 +169,7 @@ function AssignmentsController:StartEncounter(encounterID, encounterName)
             end
         end
     end
-    Log.debug("Starting encounter with ID: ", encounterID, encounterName)
+    Log.debug("Starting encounter with ID: '"..encounterID.."' Name: '"..encounterName.."'")
 end
 
 function AssignmentsController:EndEncounter()
